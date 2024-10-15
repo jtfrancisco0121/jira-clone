59b07be7066640adabf19da48559c50e648b3fd7
jtfrancisco0121 <jtfrancisco21@gmail.com>
2024-10-15 14:08:32 +0800

    Setting up Appwrite and DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 
 export default function Home() {
   return (
---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "jtdc-jira-clone-session";
