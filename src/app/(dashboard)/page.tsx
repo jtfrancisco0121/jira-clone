64e130d924a6e69de3f57a42ad39f405d37e7eed
jtfrancisco0121 <jtfrancisco21@gmail.com>
2024-10-16 21:09:43 +0800

    Dashboard Layout

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 
@@ -11,7 +10,7 @@ export default async function Home() {
 
   return (
     <div>
-      <UserButton />
+      This is a home page
     </div>
   );
 };
